a294c6a349ff89e2ce7f446df0b9cf05399881ca
Jeremiah Martin <urlimitlessaf@gmail.com>
2022-07-21 08:20:10 -0600

    update api key

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url),{
         headers: {
-                'X-RapidAPI-Key': 'fb9a84dc31msh6c3bb3b52065745p17b4f9jsnc8636ab942fe',
+                'X-RapidAPI-Key': 'YOUR_API_KEY',
                 'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
             }
     });
